93a65b514ddd9b3930bc35d20ca8b032bb53a7ef
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-05-04 22:35:32 +0530

    fw22_0703_day2

---
+++ Frontend/scripts/nav.js
@@ -0,0 +1,10 @@
+const navBar = `
+<div id="nav-btns">
+<a href="./dashboard.html">Dashboard</a>
+<a href="./adminProducts.html">Products</a>
+<a href="./userInfo.html">Users</a>
+<a href="./admin.html">Log out</a>
+</div>
+`;
+
+export { navBar };
