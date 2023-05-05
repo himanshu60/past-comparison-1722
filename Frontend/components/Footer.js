2622fbe26899e10e488c145cf8b266bc4a290f9a
Ankita <maddeshiyaaman5@gmail.com>
2023-05-05 12:26:47 +0530

    ankita_maddheshiya_fpo7_291

---
+++ Frontend/components/Footer.js
@@ -0,0 +1,23 @@
+let Footer = () =>{
+    return`
+    <div id="footer">
+        <div class="footer-container">
+          <div class="copyright">
+            <p>
+              instagram twitter facebook 2023 © CAVINKARE PRIVATE LIMITED – TRENDS
+              DIVISION
+              <br />
+              SITE BY ACODEZ
+            </p>
+          </div>
+          <div class="social-media">
+            <img id="insta" src="https://www.mygreentrends.in/wp-content/themes/greentrends/images/instagram.svg" alt="">
+            <img id="twitter" src="https://www.mygreentrends.in/wp-content/themes/greentrends/images/twitter.svg" alt="">
+            <img id="fb" src="https://www.mygreentrends.in/wp-content/themes/greentrends/images/facebook.svg" alt="">
+          </div>
+        </div>
+      </div>
+    `
+}
+
+export default Footer
